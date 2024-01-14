b53481d303860ff5353c0f7ad4abaa6760523dea
StefanoND <sndobbin@gmail.com>
2024-01-14 16:34:00 +0000

    update

--- .emacs-profiles.el
+++ .emacs-profiles.el
@@ -1,8 +1,6 @@
 (("default"   . ((user-emacs-directory   . "~/dotfiles/emacs/.emacs.d")
                  (server-name . "gnu")))
- ("stemacs"   . ((user-emacs-directory   . "~/dotfiles/emacs/.stemacs.d") ; cloned repo
-                 (server-name . "stemacs")
-                 (env . (("STEMACSDIR"   . "~/dotfiles/emacs/stemacs"))))) ; your config
+ ("stemacs"   . ((user-emacs-directory   . "~/dotfiles/emacs/.stemacs.d"))) ; cloned repo
  ("doom"      . ((user-emacs-directory   . "~/dotfiles/emacs/.doom.d") ; cloned repo
                  (server-name . "doom")
                  (env . (("DOOMDIR"      . "~/dotfiles/emacs/doomemacs"))))) ; your config
